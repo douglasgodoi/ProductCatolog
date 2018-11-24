1762f93b6428bbaf105c65b51ea1bf5c13ed206e
Douglas Ribeiro <douglas-godoi@outlook.com>
2018-11-24 18:21:55 -0200

    Initial - Curso do 1744 do balta

---
+++ Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using ProductCatalog.Data;
+using ProductCatalog.Repositories;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace ProductCatalog
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddResponseCompression();
+            services.AddScoped<StoreDataContext, StoreDataContext>();
+            services.AddTransient<ProductRepository, ProductRepository>();
+            services.AddSwaggerGen(x => {
+                x.SwaggerDoc("v1", new Info { Title = "Produtos e Categoria", Version="v1"});
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            
+            app.UseMvc();
+            app.UseResponseCompression();   
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Produtos Api - V1");
+            });
+        }
+    }
+}
